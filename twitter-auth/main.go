8599b2bb8e7807c05271c72d4dc011b058ba648a
Kazuki Sawada <kazuki@6715.jp>
2019-04-19 17:25:05 +0900

    mv authenticator

---
+++ twitter-auth/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/mrjones/oauth"
+)
+
+type (
+	TwitterAuth struct {
+		ConsumerKey       string `json:"consumer_key"`
+		ConsumerSecret    string `json:"consumer_secret"`
+		AccessTokenKey    string `json:"access_token_key"`
+		AccessTokenSecret string `json:"access_token_secret"`
+	}
+)
+
+func main() {
+	data, err := ioutil.ReadFile("auth.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var auth TwitterAuth
+	if err := json.Unmarshal(data, &auth); err != nil {
+		panic(err)
+	}
+
+	consumer := oauth.NewConsumer(
+		auth.ConsumerKey,
+		auth.ConsumerSecret,
+		oauth.ServiceProvider{
+			RequestTokenUrl:   "https://api.twitter.com/oauth/request_token",
+			AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
+			AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
+		},
+	)
+
+	requestToken, url, err := consumer.GetRequestTokenAndUrl("oob")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(url)
+
+	var pin string
+	fmt.Scanln(&pin)
+
+	accessToken, err := consumer.AuthorizeToken(requestToken, pin)
+	if err != nil {
+		panic(err)
+	}
+
+	auth.AccessTokenKey = accessToken.Token
+	auth.AccessTokenSecret = accessToken.Secret
+
+	data, err = json.Marshal(auth)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := ioutil.WriteFile("../auth/twitter.json", data, 0644); err != nil {
+		panic(err)
+	}
+}
