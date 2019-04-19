56919477bb13c5b19a2176e475803fec54a9616f
Kazuki Sawada <kazuki@6715.jp>
2019-04-19 17:23:22 +0900

    use node-fetch instead of puppeteer

--- update.js
+++ update.js
@@ -1,10 +1,14 @@
 "use strict";
 
-const puppeteer = require("puppeteer");
+const fetch = require("fetch-cookie")(require("node-fetch"));
+const cheerio = require("cheerio");
 const Twitter = require("twitter");
 const Promise = require("bluebird");
 
-const client = Promise.promisifyAll(new Twitter(require("./auth.json")));
+const kot = require("./auth/kot.json");
+const kotServer = "https://s3.kingtime.jp";
+
+const client = Promise.promisifyAll(new Twitter(require("./auth/twitter.json")));
 
 const states = ["🏠", "👨🏻‍💻"];
 const clocks = [
@@ -13,36 +17,29 @@ const clocks = [
 ];
 
 (async () => {
-	const browser = await puppeteer.launch({args: ["--no-sandbox"]});
+	const login = await fetch(`${kotServer}/admin/?page_id=/login/login_form&action_id=1&login_id=${kot.id}&login_password=${kot.password}`);
+	const $login = cheerio.load(await login.text());
+	const [_, path] = $login("meta[http-equiv=refresh]").attr("content").split("URL=");
 
-	const page = await browser.newPage();
-	await page.goto("https://s3.kingtime.jp/admin/");
-	await page.type("#login_id", "DUMMY_ID");
-	await page.type("#login_password", "DUMMY_PASSWORD");
-	await page.click("#login_button");
-	await page.waitForSelector("#footer_main_menu");
+	const resp = await fetch(`${kotServer}${path}`);
+	const $ = cheerio.load(await resp.text());
 
-	let {diff, hour, minute, count} = await page.evaluate(() => {
-		let diff = 0;
-		$(".htBlock-adjastableTableF td.all_work_time").each((_, elm) => {
-			const val = parseFloat($(elm).text());
-			if(!isNaN(val)){
-				diff += val - 8.0;
-			}
-		});
-
-		let hour, minute, count = 0;
-		$(".htBlock-adjastableTableF td.start_end_timerecord").each((_, elm) => {
-			const match = $(elm).text().match(/(\d\d):(\d\d)/);
-			if(match){
-				[_, hour, minute] = match;
-				count++;
-			}
-		});
-
-		return {diff, hour, minute, count};
+	let diff = 0;
+	$(".htBlock-adjastableTableF td.all_work_time").each((_, elm) => {
+		const val = parseFloat($(elm).text());
+		if(!isNaN(val)){
+			diff += val - 8.0;
+		}
 	});
-	await browser.close();
+
+	let hour, minute, count = 0;
+	$(".htBlock-adjastableTableF td.start_end_timerecord").each((_, elm) => {
+		const match = $(elm).text().match(/(\d\d):(\d\d)/);
+		if(match){
+			[_, hour, minute] = match;
+			count++;
+		}
+	});
 
 	hour = parseInt(hour);
 	minute = parseInt(minute);
