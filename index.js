3a3e33d7323c9e6c875cc6a910b10354197b64e2
Kazuki Sawada <kazuki@6715.jp>
2020-01-28 12:46:09 +0900

    impl: mastodon module

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const kot = require("./lib/kot");
+const mastodon = require("./lib/mastodon");
 const twitter = require("./lib/twitter");
 
 exports.run = async () => {
 	const data = await kot.calculate();
 
 	return Promise.all([
+		mastodon.update(data),
 		twitter.update(data),
 	]);
 };
